2a403b3fc928080bce631e0748777d59a380a865
Ramiz Ahmed <ramiz.ahmed.programmer@gmail.com>
2019-10-23 17:05:52 +0500

    Add transition

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import Layout from './Layout';
 import ProductCreate from './ProductCreate';
@@ -8,9 +9,22 @@ import ProductEdit from './ProductEdit';
 const App = () => {
   return (
     <BrowserRouter>
-      <Route path='/' exact component={Layout} />
-      <Route path='/new' component={ProductCreate} />
-      <Route path='/edit/:id' component={ProductEdit} />
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={3000} classNames='fade'>
+              <Switch location={location}>
+                <Route path='/' exact component={Layout} />
+
+                <div className='page'>
+                  <Route path='/new' exact component={ProductCreate} />
+                  <Route path='/edit/:id' exact component={ProductEdit} />
+                </div>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </BrowserRouter>
   );
 };
