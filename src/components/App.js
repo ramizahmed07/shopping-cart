728d6c90c169196fa44f8d4393f819f8dfe7cca1
Ramiz Ahmed <ramiz.ahmed.programmer@gmail.com>
2019-11-21 18:32:04 +0500

    Optimize app with lazy loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React, { lazy, Suspense } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { Spinner } from "reactstrap";
 
-import Layout from './Layout';
-import ProductCreate from './ProductCreate';
-import ProductEdit from './ProductEdit';
+const Layout = lazy(() => import("./Layout"));
+const ProductCreate = lazy(() => import("./ProductCreate"));
+const ProductEdit = lazy(() => import("./ProductEdit"));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route
-        render={({ location }) => (
-          <TransitionGroup>
-            <CSSTransition key={location.key} timeout={700} classNames='fade'>
-              <Switch location={location}>
-                <Route path='/' exact component={Layout} />
+      <Suspense
+        fallback={
+          <Spinner
+            style={{
+              display: "block",
+              margin: "350px auto",
+              width: "3rem",
+              height: "3rem"
+            }}
+          />
+        }
+      >
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={700} classNames="fade">
+                <Switch location={location}>
+                  <Route path="/" exact component={Layout} />
 
-                <Route path='/new' exact component={ProductCreate} />
-                <Route path='/edit/:id' exact component={ProductEdit} />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )}
-      />
+                  <Route path="/new" exact component={ProductCreate} />
+                  <Route path="/edit/:id" exact component={ProductEdit} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
+      </Suspense>
     </BrowserRouter>
   );
 };
